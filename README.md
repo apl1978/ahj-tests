d4ebf9cc9aacbdfe0870adb2c72bcfaeaf468a5b
apl1978 <apl_1978@yahoo.com>
2024-04-02 15:01:28 +0300

    added e2e tests

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/6t5g56e8baii5s7r?svg=true)](https://ci.appveyor.com/project/apl1978/ahj-events)
+[![Build status](https://ci.appveyor.com/api/projects/status/r6iedji188iwm0lp?svg=true)](https://ci.appveyor.com/project/apl1978/ahj-tests)
 
-Собранное домашнее задание на Github Pages: https://apl1978.github.io/ahj-events/
+Собранное домашнее задание на Github Pages: https://apl1978.github.io/ahj-tests/
---
+++ e2e/start.test.js
@@ -0,0 +1,26 @@
+import puppeteer from 'puppeteer';
+
+describe('Page start', () => {
+  let browser;
+  let page;
+
+  beforeEach(async () => {
+    browser = await puppeteer.launch({
+      headless: true,
+      slowMo: 100,
+      devtools: true,
+    });
+
+    page = await browser.newPage();
+  });
+
+  test('test', async () => {
+    await page.goto('http://localhost:8080');
+
+    await page.waitForSelector('body');
+  });
+
+  afterEach(async () => {
+    await browser.close();
+  });
+});
